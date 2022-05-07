2dcc10fd6c765cc968d2aa3d4ce1f47d740e7f83
j-grgur-hci <josipa3371@gmail.com>
2022-05-07 23:58:42 +0200

    added 'vremenska i prostorna slozenost' part, still needs working on'

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import PathFind from './grid'
 import Navigation from './navigation'
 import Pseudokod from './Pseudokod'
 import Primjer from './primjer'
+import Slozenost from './slozenost'
 const App=()=>{
    return( <div>
      <PathFind/>
      <Navigation/>
      <Pseudokod/>
      <Primjer/>
+     <Slozenost/>
     </div>)
 }
 export default App;
