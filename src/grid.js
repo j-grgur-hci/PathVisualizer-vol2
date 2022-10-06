b733b23f90232738fa9afcd53577d9a862479e13
j-grgur-hci <josipa3371@gmail.com>
2022-10-06 22:05:15 +0200

    Worked on scroll to top

--- src/grid.js
+++ src/grid.js
@@ -638,7 +638,15 @@ const PathFind = () => {
       document.getElementById("pseudo_lista").innerHTML = ""
     })
   }
+const toTop = document.querySelector(".to-top")
 
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 500) {
+    toTop.classList.add("active")
+  } else {
+    toTop.classList.remove("active")
+  }
+})
   return (
     <div>
       <div class="main" id="grid">
